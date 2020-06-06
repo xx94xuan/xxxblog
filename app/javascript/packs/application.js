bc191b3cf1f5ef502b92039b86bced88a76db3e4
Xiaoxuan Xue <xx94xuan@gmail.com>
2020-06-06 21:36:53 +0800

    add Redactor and remove tinymce

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
-require("./tinymce.min")
+require("./jquery-1.9.0.min")
+require("./redactor/redactor")
+require("./redactor/redactor.min")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
