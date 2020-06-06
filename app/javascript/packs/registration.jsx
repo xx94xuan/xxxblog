cbcd9c8ebcce36163095383d8d2a0d39d4da6189
Xiaoxuan Xue <xx94xuan@gmail.com>
2020-06-06 21:39:08 +0800

    change location of react component

--- app/javascript/packs/registration.jsx
+++ app/javascript/packs/registration.jsx
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import LinkButton from '../bundles/components/LinkButton';
+import LinkButton from './components/LinkButton';
 
 // This is how react_on_rails can see the Components in the browser.
 ReactOnRails.register({
-    LinkButton,
+  LinkButton,
 });
