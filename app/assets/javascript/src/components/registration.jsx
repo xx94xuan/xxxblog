ee0007563c1606c82ed0bb23b53f82b87f47d340
Xiaoxuan Xue <xx94xuan@gmail.com>
2020-11-29 22:43:49 +0800

    Use webpack.config.js instead of webpacker to compile assets

--- app/assets/javascript/src/components/registration.jsx
+++ app/assets/javascript/src/components/registration.jsx
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import LinkButton from './components/LinkButton';
+import LinkButton from './LinkButton.jsx';
 
 // This is how react_on_rails can see the Components in the browser.
 ReactOnRails.register({
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var config = {
+  context: path.resolve(__dirname, 'app', 'assets', 'javascript'),
+  entry: {
+    newPiece: [
+      "./src/jquery-1.9.0.min.js",
+      "./src/redactor/redactor.min.js"
+    ],
+    indexPieces: [
+      './src/components/registration.jsx'
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascript', 'bundle'),
+    filename: '[name]-bundle.js'
+  },
+  module: {
+    rules: [
+        {
+          test: /\.(jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+    ]
+  }
+};
+
+module.exports = config;
