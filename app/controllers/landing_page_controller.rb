c33234f966adf08e556da84a2e653353292553f9
Xiaoxuan Xue <xx94xuan@gmail.com>
2021-10-27 23:49:32 +0800

    Use redis as cache store

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,7 +2,11 @@ class LandingPageController < ApplicationController
   layout 'main'
 
   def index
-    @pieces = Piece.published.all.order('created_at DESC')
+    @pieces = Rails.cache.fetch('all_pieces', expires_in: 1.hour) do
+      Piece.published.all.order('created_at DESC')
+    end
+    # @pieces = Piece.published.all.order('created_at DESC')
+
   end
 
   def public_piece_show
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,14 +18,17 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :redis_cache_store, {url: ENV['REDIS_URL']}
+
+    # config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    # config.cache_store = :null_store
+    config.cache_store = :redis_cache_store, {url: ENV['REDIS_URL']}
   end
 
   # Don't care if the mailer can't send.
